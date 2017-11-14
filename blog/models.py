56b2ddf05d85780d5f0e83defd532a065485bda2
Tony133 <a.tripodi133@gmail.com>
2017-11-14 22:18:48 +0100

    Add Category

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, db_index=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
+    category = models.ForeignKey('Category', null=True, blank=True)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
